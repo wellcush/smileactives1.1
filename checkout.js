3e7239ce7f0d21b159dcdf47670e80fc8b56c0af
nexcomp23 <140401735+nexcomp23@users.noreply.github.com>
2023-08-01 14:04:01 -0500

    Add files via upload

---
+++ checkout.js
@@ -0,0 +1,84 @@
+let shippingInfo = {
+    fullName: '',
+    email: '',
+    phone: '',
+    address: '',
+    city: '',
+    zip: '',
+    country: '',
+    shippingMethod: 'standard',
+    nameOnCard: '',
+    cardNumber: '',
+    expiry: '',
+    cvv: '',
+};
+
+function handleInputChange(event, key) {
+    shippingInfo[key] = event.target.value;
+}
+
+function handleRadioChange(event) {
+    shippingInfo.shippingMethod = event.target.value;
+}
+
+function validateCardNumber(input) {
+    let cardNumber = input.value.replace(/\D/g, '').substring(0, 16);
+    cardNumber = cardNumber != '' ? cardNumber.match(/.{1,4}/g).join(' ') : '';
+    input.value = cardNumber;
+    shippingInfo.cardNumber = cardNumber;
+}
+
+function validateExpiry(input) {
+    let expiryDate = input.value.replace(/\D/g, '').substring(0, 4);
+    expiryDate = expiryDate != '' ? expiryDate.match(/.{1,2}/g).join('/') : '';
+    input.value = expiryDate;
+    shippingInfo.expiry = expiryDate;
+}
+
+function validateCVV(input) {
+    let cvv = input.value.replace(/\D/g, '').substring(0, 3);
+    input.value = cvv;
+    shippingInfo.cvv = cvv;
+}
+
+document.getElementById('checkout-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    let isValidForm = true;
+    for(let key in shippingInfo){
+        if(!shippingInfo[key]){
+            isValidForm = false;
+            break;
+        }
+    }
+    
+    if(isValidForm){
+        document.getElementById('checkout-form').submit();
+    } else {
+        alert("Please fill out all the fields!");
+    }
+});
+
+const inputFields = ['fullName', 'email', 'phone', 'address', 'city', 'zip', 'country', 'nameOnCard', 'cardNumber', 'expiry', 'cvv'];
+inputFields.forEach((inputField, index) => {
+    const element = document.getElementById(inputField);
+    element.addEventListener('input', function(event) {
+        if (element.id === 'cardNumber') {
+            validateCardNumber(element);
+        } else if (element.id === 'expiry') {
+            validateExpiry(element);
+        } else if (element.id === 'cvv') {
+            validateCVV(element);
+        }
+        handleInputChange(event, element.id);
+        if (element.value.length === (element.maxLength || element.size)) {
+            const nextInput = inputFields[index + 1];
+            if (nextInput) {
+                document.getElementById(nextInput).focus();
+            }
+        }
+    });
+});
+
+document.getElementById('standardShipping').addEventListener('change', handleRadioChange);
+document.getElementById('priorityShipping').addEventListener('change', handleRadioChange);
